9bb0e53cd95acadd5bf8f296d50d5c7d27bd436d
Cotang01 <127985804+Cotang01@users.noreply.github.com>
2024-02-08 17:23:36 +0500

    add src with adding nums func

---
+++ src/calc.py
@@ -0,0 +1,10 @@
+
+
+def add_numbers(n1: int | float, n2: int | float) -> int | float:
+    n1_float_len = 0
+    n2_float_len = 0
+    if isinstance(n1, float):
+        n1_float_len = len(str(n1).split('.')[-1])
+    if isinstance(n1, float):
+        n2_float_len = len(str(n2).split('.')[-1])
+    return round(n1 + n2, max(n1_float_len, n2_float_len))
