365e72155ea1b73fbbdbb2fb8237884bbdadd32a
Cotang01 <127985804+Cotang01@users.noreply.github.com>
2024-02-08 17:28:52 +0500

    fix calc.add_numbers logic

--- src/calc.py
+++ src/calc.py
@@ -5,6 +5,6 @@ def add_numbers(n1: int | float, n2: int | float) -> int | float:
     n2_float_len = 0
     if isinstance(n1, float):
         n1_float_len = len(str(n1).split('.')[-1])
-    if isinstance(n1, float):
+    if isinstance(n2, float):
         n2_float_len = len(str(n2).split('.')[-1])
     return round(n1 + n2, max(n1_float_len, n2_float_len))
